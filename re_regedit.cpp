cc71f9a8d998b31a91af8135486709309b9e5d23
Konstantin <62324477+devCarrentine@users.noreply.github.com>
2022-11-03 13:18:22 +0500

    minor cosmetic changes

--- re_regedit.cpp
+++ re_regedit.cpp
@@ -1,6 +1,6 @@
 #include "re_regedit.h"
 
-HKEY RegEdit::extractKeyHandle(Key key) {
+HKEY RegEdit::extractKeyHandle(const Key key) {
   switch (key) {
   case Key::KEY_CLASSES_ROOT:
     return HKEY_CLASSES_ROOT;
@@ -13,28 +13,29 @@ HKEY RegEdit::extractKeyHandle(Key key) {
   case Key::KEY_USERS:
     return HKEY_USERS;
   default:
-    throw std::system_error{ERROR_INVALID_HANDLE, std::system_category(),
+    throw std::system_error{ERROR_INVALID_HANDLE, std::system_category{},
                             "The specified key handle was not found"};
   }
 }
 
-RegEdit::RegEdit(Key key, const std::string &catalog) {
+RegEdit::RegEdit(const Key key, const std::string &catalog) {
   this->createCatalog(key, catalog);
 }
 
 RegEdit::~RegEdit() { this->closeCatalog(); }
 
-void RegEdit::createCatalog(Key key, const std::string &catalog) {
-  if (isOpened_)
+void RegEdit::createCatalog(const Key key, const std::string &catalog) {
+  if (isOpened_) {
     return;
+  }
 
-  HKEY keyHandle{this->extractKeyHandle(key)};
+  const HKEY keyHandle{this->extractKeyHandle(key)};
 
-  auto result{RegCreateKeyExA(keyHandle, catalog.c_str(), 0, nullptr,
+  const auto result = RegCreateKeyExA(keyHandle, catalog.c_str(), 0, nullptr,
                               REG_OPTION_NON_VOLATILE, KEY_ALL_ACCESS, nullptr,
-                              &keyHandle_, nullptr)};
+                              &keyHandle_, nullptr);
   if (result != ERROR_SUCCESS) {
-    throw std::system_error{result, std::system_category(), __FUNCTION__};
+    throw std::system_error{result, std::system_category{}, __FUNCTION__};
   }
 
   isOpened_ = true;
@@ -45,23 +46,23 @@ void RegEdit::queryValue(const std::string &key,
   DWORD type{};
   DWORD size{};
 
-  auto result{RegQueryValueExA(keyHandle_, key.c_str(), nullptr, &type, nullptr,
-                               &size)};
+  auto result = RegQueryValueExA(keyHandle_, key.c_str(), nullptr, &type, nullptr,
+                               &size);
   if (result != ERROR_SUCCESS && result != ERROR_MORE_DATA) {
     throw std::system_error{result, std::system_category()};
   }
 
-  QueriedValue queriedValue{size, type};
-  void *value = callback(queriedValue);
-  if (value == nullptr) {
+  const QueriedValue queriedValue{size, type};
+  const void *value{callback(queriedValue)};
+  if (!value) {
     return;
   }
 
-  LPBYTE valueConv{reinterpret_cast<LPBYTE>(value)};
+  const LPBYTE valueConv{reinterpret_cast<LPBYTE>(value)};
   result = RegQueryValueExA(keyHandle_, key.c_str(), nullptr, &type, valueConv,
                             &size);
   if (result != ERROR_SUCCESS) {
-    throw std::system_error{result, std::system_category()};
+    throw std::system_error{result, std::system_category{}};
   }
 }
 
@@ -72,8 +73,8 @@ const std::string RegEdit::getString(const std::string &key) {
       key, [&](const QueriedValue &queriedValue) {
         if (queriedValue.type != REG_SZ) {
           throw std::system_error{ERROR_DATATYPE_MISMATCH,
-                                  std::system_category(),
-                                  "Readed data type mismatch"};
+                                  std::system_category{},
+                                  "Read data type mismatch"};
         }
 
         value.resize(queriedValue.size);
@@ -90,8 +91,8 @@ const unsigned long RegEdit::getUlong(const std::string &key) {
       key, [&](const QueriedValue &queriedValue) {
         if (queriedValue.type != REG_DWORD) {
           throw std::system_error{ERROR_DATATYPE_MISMATCH,
-                                  std::system_category(),
-                                  "Readed data type mismatch"};
+                                  std::system_category{},
+                                  "Read data type mismatch"};
         }
         return &value;
       });
@@ -103,10 +104,10 @@ void RegEdit::setString(const std::string &key, const std::string &value) {
   const LPBYTE valueConv{(const LPBYTE)value.c_str()};
   const DWORD size{static_cast<DWORD>(value.length())};
 
-  auto result{
-      RegSetValueExA(keyHandle_, key.c_str(), 0, REG_SZ, valueConv, size)};
+  const auto result =
+      RegSetValueExA(keyHandle_, key.c_str(), 0, REG_SZ, valueConv, size);
   if (result != ERROR_SUCCESS) {
-    throw std::system_error{result, std::system_category(), __FUNCTION__};
+    throw std::system_error{result, std::system_category{}, __FUNCTION__};
   }
 }
 
@@ -114,36 +115,36 @@ void RegEdit::setUlong(const std::string &key, const unsigned long value) {
   const LPBYTE valueConv{(const LPBYTE)&value};
   const DWORD size{sizeof(unsigned long)};
 
-  auto result{
-      RegSetValueExA(keyHandle_, key.c_str(), 0, REG_DWORD, valueConv, size)};
+  const auto result =
+      RegSetValueExA(keyHandle_, key.c_str(), 0, REG_DWORD, valueConv, size);
   if (result != ERROR_SUCCESS) {
-    throw std::system_error{result, std::system_category(), __FUNCTION__};
+    throw std::system_error{result, std::system_category{}, __FUNCTION__};
   }
 }
 
 void RegEdit::removeValue(const std::string &key) {
-  auto result{RegDeleteValueA(keyHandle_, key.c_str())};
+  const auto result = RegDeleteValueA(keyHandle_, key.c_str());
   if (result != ERROR_SUCCESS) {
-    throw std::system_error{result, std::system_category()};
+    throw std::system_error{result, std::system_category{}};
   }
 }
 
 void RegEdit::removeCatalog(Key key, const std::string &catalog) {
   HKEY keyHandle{this->extractKeyHandle(key)};
 
-  auto result{RegDeleteKeyExA(keyHandle, catalog.c_str(), KEY_WOW64_32KEY, 0)};
+  const auto result = RegDeleteKeyExA(keyHandle, catalog.c_str(), KEY_WOW64_32KEY, 0);
   if (result != ERROR_SUCCESS) {
-    throw std::system_error{result, std::system_category(), __FUNCTION__};
+    throw std::system_error{result, std::system_category{}, __FUNCTION__};
   }
 }
 
 void RegEdit::closeCatalog() {
-  if (!isOpened_)
+  if (!isOpened_) {
     return;
+  }
 
-  auto result{RegCloseKey(keyHandle_)};
-  if (result != ERROR_SUCCESS) {
-    throw std::system_error{result, std::system_category(), __FUNCTION__};
+  if (RegCloseKey(keyHandle_) != ERROR_SUCCESS) {
+    throw std::system_error{result, std::system_category{}, __FUNCTION__};
   }
 
   isOpened_ = false;
